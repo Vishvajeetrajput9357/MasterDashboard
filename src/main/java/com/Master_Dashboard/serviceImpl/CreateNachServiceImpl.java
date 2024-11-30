b43defb6e737651b38871d7c8ff66c55563670e8
Vishvajeet_rajput <jeet769826@gmail.com>
2024-11-30 20:10:47 +0530

    ENach Redirect lodding changes 2.8

--- src/main/java/com/Master_Dashboard/serviceImpl/CreateNachServiceImpl.java
+++ src/main/java/com/Master_Dashboard/serviceImpl/CreateNachServiceImpl.java
@@ -2,39 +2,24 @@ package com.Master_Dashboard.serviceImpl;
 
 import java.util.HashMap;
 import java.util.Map;
-
-//import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
 import com.Master_Dashboard.request.CreateMandateRequest;
 import com.Master_Dashboard.request.ResponseMessage;
 import com.Master_Dashboard.service.CreateNachService;
 
 @Service
 public class CreateNachServiceImpl implements CreateNachService {
+	private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(CreateNachServiceImpl.class);
 
 	@Override
 	public Map<String, Object> createNach(CreateMandateRequest createMandateRequest) {
 		Map<String, Object> map = new HashMap<String, Object>();
-//	LOGGER.info(createMandateRequest.toString());
+		LOGGER.info(createMandateRequest.toString());
 		map.put(ResponseMessage.STATUS, ResponseMessage.STATUS_SUCCESS);
 		map.put(ResponseMessage.CODE, ResponseMessage.SUCCESS);
 		map.put(ResponseMessage.DESCRIPTION, ResponseMessage.MANDATE_URL_SUCCESS);
 		map.put("url", "https://empirical-tootsie-kjsstpay-6c9fa64e.koyeb.app/dashboard/nachRedirect/createMandate");
 		return map;
 	}
-
-//	private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(MerchantEkycServiceImpl.class);
-
-//	@Override
-//	public Map<String, Object> createNach(CreateMandateRequest request) {
-//		Map<String, Object> map = new HashMap<String, Object>();
-////		LOGGER.info(createMandateRequest.toString());
-//		map.put(ResponseMessage.STATUS, ResponseMessage.STATUS_SUCCESS);
-//		map.put(ResponseMessage.CODE, ResponseMessage.SUCCESS);
-//		map.put(ResponseMessage.DESCRIPTION, ResponseMessage.MANDATE_URL_SUCCESS);
-//		map.put("url","https://empirical-tootsie-kjsstpay-6c9fa64e.koyeb.app/dashboard/nachRedirect/createMandate");
-//		 return map;
-//	}
-
 }
