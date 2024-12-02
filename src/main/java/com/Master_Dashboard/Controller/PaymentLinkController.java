a10d0281491cde0d8ba1eae04332349ec0412f87
Vishvajeet_rajput <jeet769826@gmail.com>
2024-12-02 22:44:50 +0530

    Changes in Enach redirect 2.4

--- src/main/java/com/Master_Dashboard/Controller/PaymentLinkController.java
+++ src/main/java/com/Master_Dashboard/Controller/PaymentLinkController.java
@@ -96,8 +96,8 @@ public class PaymentLinkController {
 			
 //		"50100000835738|2024-11-25|2025-11-25|100.00|"
 //		String authType="Debit";
-//		String authType="NET";
-			String authType="AADHAAR";
+		String authType="NET";
+//			String authType="AADHAAR";
 			model.addObject("messageId", uniqueId);
 			model.addObject("Debit", authType);
 			model.setViewName("user/CreateNach");
